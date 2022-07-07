dcee1af2fa7912e9ca800311e9ed0fc11c8e2d04
DeysiLopes <deysilinconl@gmail.com>
2022-07-07 17:25:44 -0300

    Test with methods in ruby

--- oo_ruby/principal.rb
+++ oo_ruby/principal.rb
@@ -28,3 +28,35 @@ person = Pessoa2.gerar
 person = Pessoa2.gerar
 person = Pessoa2.gerar
 p Pessoa2.numero_de_pessoas
+
+p "__________________________________________"
+puts"\n"
+
+class MinhaClasse
+    def m1
+        puts "m1"
+    end
+    private def m2
+        puts "m2"
+    end
+    def m3
+        puts "m3"
+    end
+    protected def m5
+        puts "m5"
+    end
+   
+end
+class MinhaClasse2 < MinhaClasse
+    def m4
+        m1
+        m2
+        m3
+        obj3 = MinhaClasse.new
+        puts "m4"
+        obj3.m5
+    end
+end 
+
+obj5 = MinhaClasse2.new
+obj5.m4

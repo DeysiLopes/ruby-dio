a6520a70ad82d7157a69b294546d21a42420d5f7
DeysiLopes <deysilinconl@gmail.com>
2022-07-07 17:32:13 -0300

    REFACTOR: changing varibles name in ContaCorrente

--- oo_ruby/exercicio/ContaCorrente.rb
+++ oo_ruby/exercicio/ContaCorrente.rb
@@ -1,8 +1,13 @@
 class ContaCorrente < ContasBancarias
+    TAXA = 2 #constante
+    # def self.taxa #método de classes
+    #     2
+    # end
+
     def transferir(valor, outra_conta)
         if @saldo >= valor
             super
-            retirar(2)  
+            retirar(TAXA) 
         end
     end
 

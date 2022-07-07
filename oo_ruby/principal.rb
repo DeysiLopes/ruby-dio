079070e0d0470000b7d8298d5e2a25893e8de107
DeysiLopes <deysilinconl@gmail.com>
2022-07-07 13:31:17 -0300

    Class methods and variables

--- oo_ruby/principal.rb
+++ oo_ruby/principal.rb
@@ -6,4 +6,25 @@ obj1 = Pessoa.new("Deysi")
 obj2 = Carro.new("Fusca", obj1)
 
 ap obj1
-ap obj2
+ap obj2 
+
+p "__________________________________________"
+puts"\n"
+
+class Pessoa2 
+    @@numero_de_pessoas = 0
+    def self.gerar 
+        @@numero_de_pessoas += 1
+        puts "Gerando pessoa..."
+        Pessoa2.new
+    end
+    def self.numero_de_pessoas
+        @@numero_de_pessoas
+    end
+end
+
+person = Pessoa2.gerar
+person = Pessoa2.gerar
+person = Pessoa2.gerar
+person = Pessoa2.gerar
+p Pessoa2.numero_de_pessoas
